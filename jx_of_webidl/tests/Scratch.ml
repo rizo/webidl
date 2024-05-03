eed572d75d17b3d2cb4892ce4b740bfc0b272481
Rizo I <rizo@odis.io>
2024-05-03 13:43:10 +0100

    add argument generation expect tests

--- jx_of_webidl/tests/Scratch.ml
+++ jx_of_webidl/tests/Scratch.ml
@@ -1,49 +1,131 @@
 module rec A : sig
   type t = [ `A ] Js.t
 
-  val f0 : arg1:bool -> arg2:string -> arg3:float -> t -> unit
-  val f1 : arg:[ `Bool | `String ] Js.t -> t -> unit
-  val f2 : arg1:[ `Node | `Bool | `String ] Js.t -> arg2:float -> t -> unit
-
-  val f3 :
-    arg1:[ `Node | `Bool | `String ] Js.nullable ->
-    arg2:float ->
-    arg3:[ `Bool | `Float ] Js.t ->
-    t ->
-    unit
-
-  val f4 : arg:[ `Node | `Nullable of [ `Bool ] | `String ] Js.t -> t -> unit
+  val b1 : x:bool -> t -> unit
+  val f1 : x:float -> t -> unit
+  val s1 : x:string -> t -> unit
+  val t1 : x:[ `T ] Js.t -> t -> unit
+  val a1 : x:Js.any -> t -> unit
+  val bn1 : x:bool option -> t -> unit
+  val tn1 : x:[ `T ] Js.t option -> t -> unit
+  val bo1 : ?x:bool -> t -> unit
+  val bo2 : ?x:bool -> t -> unit
+  val bo3 : ?x:bool option -> t -> unit
+  val bo4 : ?x:bool option -> t -> unit
+  val bo5 : ?x:bool option -> t -> unit
+  val to1 : ?x:[ `T ] Js.t -> t -> unit
+  val to2 : ?x:[ `T ] Js.t option -> t -> unit
+  val to3 : ?x:[ `T ] Js.t option -> t -> unit
+  val ao1 : ?x:Js.any -> t -> unit
+  val ao2 : ?x:Js.any -> t -> unit
+  val ao3 : ?x:Js.any -> t -> unit
+  val ou1 : ?x:[ `Bool | `String ] Js.t -> t -> unit
+  val ou2 : ?x:[ `Nullable of [ `Bool ] | `String ] Js.t -> t -> unit
+  val ou3 : ?x:[ `Bool | `String ] Js.nullable -> t -> unit
+  val ou4 : ?x:[ `Bool | `String ] Js.nullable -> t -> unit
+  val ou5 : ?x:[ `Bool | `String ] Js.nullable -> t -> unit
 end = struct
   type t = [ `A ] Js.t
 
-  let f0 ~arg1 ~arg2 ~arg3 this =
-    let arg1 = Js.Any.of_bool arg1 in
-    let arg2 = Js.Any.of_string arg2 in
-    let arg3 = Js.Any.of_float arg3 in
-    Js.to_unit (Js.Ffi.meth_call this "f0" [| arg1; arg2; arg3 |])
+  let b1 ~x this =
+    let x = Js.Any.of_bool x in
+    Js.to_unit (Js.Ffi.meth_call this "b1" [| x |])
 
-  let f1 ~arg this =
-    let arg = Js.to_any arg in
-    Js.to_unit (Js.Ffi.meth_call this "f1" [| arg |])
+  let f1 ~x this =
+    let x = Js.Any.of_float x in
+    Js.to_unit (Js.Ffi.meth_call this "f1" [| x |])
 
-  let f2 ~arg1 ~arg2 this =
-    let arg1 = Js.to_any arg1 in
-    let arg2 = Js.Any.of_float arg2 in
-    Js.to_unit (Js.Ffi.meth_call this "f2" [| arg1; arg2 |])
+  let s1 ~x this =
+    let x = Js.Any.of_string x in
+    Js.to_unit (Js.Ffi.meth_call this "s1" [| x |])
 
-  let f3 ~arg1 ~arg2 ~arg3 this =
-    let arg1 = Js.to_any arg1 in
-    let arg2 = Js.Any.of_float arg2 in
-    let arg3 = Js.to_any arg3 in
-    Js.to_unit (Js.Ffi.meth_call this "f3" [| arg1; arg2; arg3 |])
+  let t1 ~x this =
+    let x = Js.to_any x in
+    Js.to_unit (Js.Ffi.meth_call this "t1" [| x |])
 
-  let f4 ~arg this =
-    let arg = Js.to_any arg in
-    Js.to_unit (Js.Ffi.meth_call this "f4" [| arg |])
-end
+  let a1 ~x this =
+    let x = Js.of_any x in
+    Js.to_unit (Js.Ffi.meth_call this "a1" [| x |])
 
-and Node : sig
-  type t = [ `Node ] Js.t
-end = struct
-  type t = [ `Node ] Js.t
+  let bn1 ~x this =
+    let x = (Js.Any.nullable_of_option Js.Any.of_bool) x in
+    Js.to_unit (Js.Ffi.meth_call this "bn1" [| x |])
+
+  let tn1 ~x this =
+    let x = (Js.Any.nullable_of_option Js.to_any) x in
+    Js.to_unit (Js.Ffi.meth_call this "tn1" [| x |])
+
+  let bo1 ?x this =
+    let x = (Js.Any.undefined_of_option Js.Any.of_bool) x in
+    Js.to_unit (Js.Ffi.meth_call this "bo1" [| x |])
+
+  let bo2 ?x this =
+    let x = (Js.Any.undefined_of_option Js.Any.of_bool) x in
+    Js.to_unit (Js.Ffi.meth_call this "bo2" [| x |])
+
+  let bo3 ?x this =
+    let x =
+      (Js.Any.undefined_of_option (Js.Any.nullable_of_option Js.Any.of_bool)) x
+    in
+    Js.to_unit (Js.Ffi.meth_call this "bo3" [| x |])
+
+  let bo4 ?x this =
+    let x =
+      (Js.Any.undefined_of_option (Js.Any.nullable_of_option Js.Any.of_bool)) x
+    in
+    Js.to_unit (Js.Ffi.meth_call this "bo4" [| x |])
+
+  let bo5 ?x this =
+    let x =
+      (Js.Any.undefined_of_option (Js.Any.nullable_of_option Js.Any.of_bool)) x
+    in
+    Js.to_unit (Js.Ffi.meth_call this "bo5" [| x |])
+
+  let to1 ?x this =
+    let x = (Js.Any.undefined_of_option Js.to_any) x in
+    Js.to_unit (Js.Ffi.meth_call this "to1" [| x |])
+
+  let to2 ?x this =
+    let x =
+      (Js.Any.undefined_of_option (Js.Any.nullable_of_option Js.to_any)) x
+    in
+    Js.to_unit (Js.Ffi.meth_call this "to2" [| x |])
+
+  let to3 ?x this =
+    let x =
+      (Js.Any.undefined_of_option (Js.Any.nullable_of_option Js.to_any)) x
+    in
+    Js.to_unit (Js.Ffi.meth_call this "to3" [| x |])
+
+  let ao1 ?x this =
+    let x = (Js.Any.undefined_of_option Js.of_any) x in
+    Js.to_unit (Js.Ffi.meth_call this "ao1" [| x |])
+
+  let ao2 ?x this =
+    let x = (Js.Any.undefined_of_option Js.of_any) x in
+    Js.to_unit (Js.Ffi.meth_call this "ao2" [| x |])
+
+  let ao3 ?x this =
+    let x = (Js.Any.undefined_of_option Js.of_any) x in
+    Js.to_unit (Js.Ffi.meth_call this "ao3" [| x |])
+
+  let ou1 ?x this =
+    let x = (Js.Any.undefined_of_option Js.to_any) x in
+    Js.to_unit (Js.Ffi.meth_call this "ou1" [| x |])
+
+  let ou2 ?x this =
+    let x = (Js.Any.undefined_of_option Js.to_any) x in
+    Js.to_unit (Js.Ffi.meth_call this "ou2" [| x |])
+
+  let ou3 ?x this =
+    let x = (Js.Any.undefined_of_option Js.to_any) x in
+    Js.to_unit (Js.Ffi.meth_call this "ou3" [| x |])
+
+  let ou4 ?x this =
+    let x = (Js.Any.undefined_of_option Js.to_any) x in
+    Js.to_unit (Js.Ffi.meth_call this "ou4" [| x |])
+
+  let ou5 ?x this =
+    let x = (Js.Any.undefined_of_option Js.to_any) x in
+    Js.to_unit (Js.Ffi.meth_call this "ou5" [| x |])
 end
--- jx_of_webidl/tests/Scratch.webidl
+++ jx_of_webidl/tests/Scratch.webidl
@@ -1,11 +1,30 @@
 interface A {
-  undefined f0(boolean arg1, DOMString arg2, float arg3);
-  undefined f1((boolean or DOMString) arg);
-  undefined f2((Node or boolean or DOMString) arg1, float arg2);
-  undefined f3((Node or boolean or DOMString)? arg1, float arg2, (boolean or float) arg3);
-  undefined f4((Node or boolean? or DOMString) arg);
+  undefined b1(boolean x);
+  undefined f1(float x);
+  undefined s1(DOMString x);
+  undefined t1(T x);
+  undefined a1(any x);
+  undefined bn1(boolean? x);
+  undefined tn1(T? x);
+  //undefined u1((boolean or DOMString) x);
+  //undefined u2((boolean? or DOMString) x);
+  //undefined u3((boolean or DOMString)? x);
+
+  undefined bo1(optional boolean x);
+  undefined bo2(optional boolean x = false);
+  undefined bo3(optional boolean? x);
+  undefined bo4(optional boolean? x = null);
+  undefined bo5(optional boolean? x = false);
+  undefined to1(optional T x);
+  undefined to2(optional T? x);
+  undefined to3(optional T? x = null);
+  undefined ao1(optional any x);
+  undefined ao2(optional any x = true);
+  undefined ao3(optional any x = null);
+  undefined ou1(optional (boolean or DOMString) x);
+  undefined ou2(optional (boolean? or DOMString) x = null);
+  undefined ou3(optional (boolean or DOMString)? x);
+  undefined ou4(optional (boolean or DOMString)? x = null);
+  undefined ou5(optional (boolean or DOMString)? x = "abc");
 };
 
-interface Node {
-  
-};
